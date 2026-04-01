dc08d5f971ea02ef06f8cc44c09ba6c7b29120e3
João Pedro Moro Giordani <jpmorogiordani@gmail.com>
2026-04-01 18:08:55 -0300

    First commit: Mobile Movie App

---
+++ services/api.ts
@@ -0,0 +1,42 @@
+export const TMDB_CONFIG = {
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+    }
+}
+
+export const fetchMovies = async ({query}: {query: string}) => {
+    const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?quey=${query}` :
+    `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+    const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: TMDB_CONFIG.headers,
+    })
+
+    if  (!response.ok) {
+        // @ts-ignore
+        throw new Error('Failed to fetch movies', response.statusText);
+    }
+
+    const data = await response.json();
+
+    return data.results
+}
+
+// const url = 'https://api.themoviedb.org/3/keyword/keyword_id/movies';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NDRjMDUzYTZkODRiZDk3MWI5ZDEwY2Q2OTIwMDBjNiIsIm5iZiI6MTc3NTA3NDQyNC43NTksInN1YiI6IjY5Y2Q3Yzc4Nzc5ODJkMGVkNzljNzc1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cSSIVl4AcWw6q_sIsETvI1Ul11UJrIaq42vlqIC0ECc'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
